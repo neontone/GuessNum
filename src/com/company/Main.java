517d20f9f1018096fb02b7d388ee6e61a5e10ed2
neontone <antons.korenevskis@gmail.com>
2019-10-01 19:27:23 +0300

    added y/n

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,34 +4,64 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+
     static Random rand = new Random();
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int myNum = rand.nextInt(100) + 1;
-        boolean userWon = false;
         String answer;
-        for (int i = 0; i < 10; i++)
-            do {
 
-                    System.out.println("Enter your guess");
-                    int userNum = scan.nextInt();
+        do {
+            int myNum = rand.nextInt(100) + 1;
+            boolean userWon = false;
 
+            for (int i = 0; i < 10; i++) {
+                int userNum = askInt("Please, enter your guess:", 1, 100);
 
-                    if (myNum == userNum) {
-                    System.out.println("Yes!");
+                if (myNum > userNum) {
+                    System.out.println("My number is greater than yours");
+                } else if (myNum < userNum) {
+                    System.out.println("My number is less than yours");
+                } else {
+                    System.out.println("You won!");
                     userWon = true;
                     break;
-                    } else if (myNum > userNum) {
-                    System.out.println("No! Its greater than yours!");
-                     } else {
-                    System.out.println("No! Its less than yours");
-                    }
-                    if (!userWon) {
-                    System.out.println("Loser!");
-                    }
-                    System.out.println("New game? (Y/N)");
-                    answer = scan.next();
-                } while (answer.equals("Y"));
+                }
+            }
+
+            if (!userWon) {
+                System.out.println("Loser!");
+            }
+
+            answer = askYesNo("Do you want to play again? (Y/N)");
+
+        } while (answer.equalsIgnoreCase("y"));
+
+        System.out.println("Bye!");
+    }
+
+    static int askInt(String msg, int min, int max) {
+        for (; ; ) {
+            System.out.println(msg);
+            int answer = scan.nextInt();
+            if (answer >= min && answer <= max) {
+                return answer;
+            }
+            System.out.printf("Please enter number from %d to %d\n", min, max);
+        }
+    }
+
+    static String askYesNo(String msg)
+
+    {
+        while (true) {
+            System.out.println(msg);
+            String answer = scan.next();
+            if (answer.equalsIgnoreCase("y")
+                    || answer.equalsIgnoreCase("n")) {
+                return answer;
+            }
+            System.out.println("Enter 'y' or 'n'");
+        }
     }
 }
